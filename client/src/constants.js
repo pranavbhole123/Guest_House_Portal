876ef1f1bf19283a302f50d24a82a8f55b16e567
pranav bhole <pranavmasterscience@gmail.com>
2025-03-07 13:46:30 +0530

    final deploy

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,4 +2,4 @@
 // export const BASE_URL = "http://localhost:4751";
 
 // Use environment variable in production or fallback to localhost for development
-export const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:4751";
+export const BASE_URL =  "https://guesthouseportal-production.up.railway.app/";
